aa5f09058cd8ee4aca11c147fead47e72d9b208e
ALJHONDUPAL <202011035@gordoncollege.edu.ph>
2024-09-16 17:01:42 +0800

    Add Google fonts and flowbite-svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>import "../app.css";</script>
 
+
+<svelte:head>
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&amp;family=Manrope:wght@200..800&amp;display=swap" rel="stylesheet" />
+</svelte:head>
 <slot></slot>
---
+++ src/routes/hello-world/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import type { PageData } from '../hello-world/$types';
+    
+    export let data: PageData;
+</script>
+
+
+<h1>hello world</h1>
