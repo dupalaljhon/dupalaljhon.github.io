19913dbc5651aead47c50d0f738b8005b88371b1
ALJHONDUPAL <202011035@gordoncollege.edu.ph>
2024-09-23 14:18:46 +0800

    try2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,4 @@
 <script>import "../app.css";</script>
-
-
 <svelte:head>
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,14 @@
 </script>
 
 <Alert>TEST</Alert>
+
+
+
+<div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <div class="bg-white rounded-lg shadow-lg p-10 max-w-md text-center">
+      <h1 class="text-4xl font-bold text-gray-800 mb-4">Welcome to my Portfolio!</h1>
+      <p class="text-gray-600 text-lg">
+        Thank you for visiting my portfolio. Feel free to explore my projects and work.
+      </p>
+    </div>
+  </div>
