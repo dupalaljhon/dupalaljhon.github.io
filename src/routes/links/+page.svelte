68577a278d57ebc41452c9d8c0cd9a6a1406ef2f
ALJHONDUPAL <202011035@gordoncollege.edu.ph>
2024-10-01 05:07:35 +0800

    links modifications

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,32 +1,33 @@
 
 <nav class="bg-gray-800 p-4">
-    <div class="container mx-auto flex justify-between items-center">
+  <div class="container mx-auto flex justify-between items-center">
       <!-- Left side: Welcome Text -->
       <div class="text-white font-bold text-lg">
-        Welcome to my portfolio
+          Welcome to my portfolio
       </div>
-  
+
       <!-- Right side: Nav Links -->
       <div class="space-x-4">
-        <a href="/" class="text-white hover:text-gray-300 font-semibold">Home</a>
-        <a href="/about" class="text-white hover:text-gray-300 font-semibold">About</a>
-        <a href="/links" class="text-white hover:text-gray-300 font-semibold">Contact Me</a>
+          <a href="/" class="text-white hover:bg-gray-700 focus:ring-4 focus:ring-gray-500 font-semibold rounded-lg px-4 py-2 transition duration-200">Home</a>
+          <a href="/about" class="text-white hover:bg-gray-700 focus:ring-4 focus:ring-gray-500 font-semibold rounded-lg px-4 py-2 transition duration-200">About</a>
+          <a href="/links" class="text-white hover:bg-gray-700 focus:ring-4 focus:ring-gray-500 font-semibold rounded-lg px-4 py-2 transition duration-200">Contact Me</a>
       </div>
-    </div>
-  </nav>
+  </div>
+</nav>
+
 
 <style>
     .social-icons {
       display: flex;
       justify-content: center;
-      margin-top: 20px;
+      margin-top: 60px;
     }
   
     .social-icons a {
-      margin: 0 15px;
+      margin: 0 55px;
       display: inline-block;
-      width: 40px;
-      height: 40px;
+      width: 90px;
+      height: 90px;
       fill: #4A5568; /* Default icon color (gray) */
       transition: fill 0.3s ease;
     }
@@ -40,30 +41,41 @@
       height: 100%;
     }
   </style>
-  
+
+<div class="flex justify-center items-center h-auto p-20">
+  <div class="text-blue font-bold text-3xl">
+    <h1>You may visit my accounts and message me by using these links below!</h1>
+  </div>
+</div>
+
   <div class="social-icons">
-    <a href="https://dupalaljhon.github.io/" target="_blank" aria-label="GitHub">
+    <a href="https://github.com/dupalaljhon" target="_blank" aria-label="GitHub">
       <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M12 0C5.37 0 0 5.37 0 12c0 5.303 3.438 9.8 8.207 11.387.6.11.793-.26.793-.577 0-.285-.01-1.04-.015-2.04-3.338.725-4.042-1.61-4.042-1.61-.546-1.387-1.334-1.757-1.334-1.757-1.09-.745.083-.729.083-.729 1.204.085 1.838 1.237 1.838 1.237 1.07 1.835 2.807 1.305 3.492.998.108-.776.418-1.305.762-1.605-2.665-.303-5.467-1.333-5.467-5.93 0-1.31.468-2.382 1.235-3.222-.123-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.301 1.23A11.516 11.516 0 0112 6.804c1.02.005 2.046.138 3.004.404 2.293-1.552 3.299-1.23 3.299-1.23.653 1.653.241 2.873.119 3.176.77.84 1.235 1.912 1.235 3.222 0 4.609-2.805 5.625-5.475 5.921.43.372.823 1.103.823 2.222 0 1.606-.015 2.902-.015 3.296 0 .32.192.694.798.576C20.565 21.796 24 17.298 24 12 24 5.37 18.63 0 12 0z"/>
       </svg>
     </a>
   
     <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=202011035@gordoncollege.edu.ph" target="_blank" aria-label="Gmail">
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path d="M21.35 11.1H12V21.9h9.35c1.27 0 2.35-1.08 2.35-2.35V13.45c0-1.27-1.08-2.35-2.35-2.35zM3 12c0 1.27 1.08 2.35 2.35 2.35H12V2.1H5.35C4.08 2.1 3 3.18 3 4.45v7.1zM12 2.1v9.25h8.65c1.27 0 2.35-1.08 2.35-2.35V4.45c0-1.27-1.08-2.35-2.35-2.35H12z" />
-        </svg>
-      </a>
-      
-    <a href="https://twitter.com/aljhondupal" target="_blank" aria-label="Twitter">
-      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M23.954 4.569c-.885.389-1.83.654-2.825.775a4.932 4.932 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482A13.978 13.978 0 011.671 3.149a4.902 4.902 0 001.523 6.573 4.903 4.903 0 01-2.229-.616v.061c0 2.385 1.693 4.374 3.946 4.827a4.935 4.935 0 01-2.224.085 4.93 4.93 0 004.604 3.417A9.868 9.868 0 010 19.54a13.91 13.91 0 007.548 2.212c9.142 0 14.307-7.721 14.307-14.415 0-.22-.004-.438-.015-.653A10.243 10.243 0 0024 4.59z"/>
+      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 50 50">
+        <path d="M 5.5 7 C 3.019531 7 1 9.019531 1 11.5 L 1 11.925781 L 25 29 L 49 11.925781 L 49 11.5 C 49 9.019531 46.980469 7 44.5 7 Z M 6.351563 9 L 43.644531 9 L 25 22 Z M 1 14.027344 L 1 38.5 C 1 40.980469 3.019531 43 5.5 43 L 44.5 43 C 46.980469 43 49 40.980469 49 38.5 L 49 14.027344 L 43 18.296875 L 43 41 L 7 41 L 7 18.296875 Z"></path>
       </svg>
     </a>
+    
   
     <a href="https://www.facebook.com/profile.php?id=100007309275458&mibextid=ZbWKwL" target="_blank" aria-label="Facebook">
       <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M22.675 0H1.325C.593 0 0 .593 0 1.326v21.348C0 23.407.593 24 1.325 24H12.82v-9.294H9.692V10.62h3.128V8.413c0-3.1 1.892-4.788 4.657-4.788 1.325 0 2.463.099 2.797.143v3.24h-1.918c-1.505 0-1.796.715-1.796 1.763v2.31h3.587l-.467 3.086h-3.12V24h6.116c.73 0 1.324-.593 1.324-1.326V1.326C24 .593 23.407 0 22.675 0z"/>
       </svg>
     </a>
+
+    <a href="https://t.me/Aljhondupal1023" target="_blank" aria-label="Telegram">
+      <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+        <!-- <title>Telegram</title> -->
+        <g data-name="telegram social media network chat" id="telegram_social_media_network_chat">
+          <path d="M28.59,4.29a2.23,2.23,0,0,0-2.27-.36L3.41,13.1a1.83,1.83,0,0,0,0,3.38l1.48.61a1,1,0,0,0,1.31-.53,1,1,0,0,0-.54-1.31L4.56,14.8l22.51-9a.22.22,0,0,1,.23,0,.24.24,0,0,1,.08.23L23.27,25.21a.4.4,0,0,1-.26.3.39.39,0,0,1-.39-.06l-8-6.24,7.83-7.91a1,1,0,0,0-1.22-1.56L9.75,16.54a1,1,0,1,0,1,1.72l4.83-2.85L13.23,17.8a2,2,0,0,0,.2,3.08l8,6.15a2.4,2.4,0,0,0,1.47.5,2.47,2.47,0,0,0,.83-.15,2.37,2.37,0,0,0,1.52-1.75L29.33,6.47A2.23,2.23,0,0,0,28.59,4.29Z"/>
+        </g>
+      </svg>
+    </a>
+    
+    
   </div>
-  
