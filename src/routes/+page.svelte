9a7be9e8345633e625fd6146fff6d4e5645249b7
ALJHONDUPAL <202011035@gordoncollege.edu.ph>
2024-10-04 17:35:28 +0800

    design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,6 @@
 <!-- <script lang="ts">
     import{Alert} from "flowbite-svelte";
-</script>
-
-<Alert>TEST</Alert>
-
- -->
+</script> -->
 
 <!-- <div class="flex items-center justify-center min-h-screen bg-gray-100">
   <div class="bg-white rounded-lg shadow-lg p-10 max-w-md text-center">
@@ -34,20 +30,40 @@
       </div>
   </div> -->
 <!-- </nav>  -->
+<!-- <script lang="ts">
+  import { Alert } from "flowbite-svelte";
+  
+</script> -->
+<div style="background-color: #3f3f46; min-height: 100vh; padding: 20px;"> <!-- Set background color and padding -->
+  
+  <div class="flex flex-col lg:flex-row items-center justify-center p-6 mt-36" style="margin-top: 10px;">
+    <div class="text-left mt-10 lg:mt-40 lg:ml-32 flex flex-col items-start" style="margin-right: 200px;">
+      <h3 class="text-2xl lg:text-4xl font-bold text-white mb-2 lg:mb-4">Hello, It's me</h3>
+      <h1 class="text-4xl lg:text-6xl font-bold text-white mb-2 lg:mb-4">AL JHON DUPAL</h1>
+      
+      <div class="overflow-hidden">
+        <h3 class="text-xl lg:text-3xl font-bold text-white mb-2 lg:mb-4 text-info">
+          And I am an Information Technology Student.
+        </h3>
+      </div>
+    </div>
 
-<div class="flex flex-col lg:flex-row items-center justify-between p-6 lg:p-0 mt-36" style="margin-top: 100px;">
-  <!-- Left side: Text Details -->
-  <div class="text-left mt-10 lg:mt-40 lg:ml-32">
-    <h3 class="text-2xl lg:text-4xl font-bold text-gray-800 mb-2 lg:mb-4">Hello, It's me</h3>
-    <h1 class="text-4xl lg:text-6xl font-bold text-gray-800 mb-2 lg:mb-4">AL JHON DUPAL</h1>
-    <h3 class="text-xl lg:text-3xl font-bold text-gray-800 mb-2 lg:mb-4">And I am an Information Technology Student</h3>
+    <div class="flex-shrink-0 mt-10 lg:mt-40 lg:mr-32">
+      <div class="relative rounded-full h-40 w-40 lg:h-80 lg:w-80">
+        <div class="absolute inset-0 bg-gradient-to-r from-blue-600 via-sky-600 to-red-900 rounded-full opacity-50 animate-pulse"></div>
+        
+        <img src="/myphoto.jpg" alt="AL JHON DUPAL" 
+             class="relative rounded-full h-40 w-40 lg:h-80 lg:w-80 shadow-lg ring-4 ring-white" 
+             style="box-shadow: 0 0 30px rgba(0, 123, 255, 0.7), 0 0 60px rgba(100, 100, 255, 0.5), 0 0 100px rgba(200, 50, 255, 0.3), 0 0 10px 10px rgba(255, 255, 255, 0.8);">
+      </div>
+    </div>
   </div>
 
-  <!-- Right side: Image with Sky Blue Shadow -->
-  <div class="flex-shrink-0 mt-10 lg:mt-24 lg:mr-32">
-    <img src="/myphoto.jpg" alt="AL JHON DUPAL" class="rounded-full h-40 w-40 lg:h-80 lg:w-80 shadow-lg ring-4 ring-sky-600" style="box-shadow: 0px 10px 30px rgba(0, 123, 255, 0.5);">
-  </div>
+  <!-- Removed the JavaScript related to typing effect -->
 </div>
+
+
+  
 
 
     
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,23 +1,23 @@
 <style>
   .social-icons {
     display: flex;
-    flex-wrap: wrap; /* Allow items to wrap on small screens */
+    flex-wrap: wrap; 
     justify-content: center;
     margin-top: 60px;
-    padding: 0 15px; /* Add padding for smaller screens */
+    padding: 0 15px; 
   }
 
   .social-icons a {
-    margin: 15px; /* Reduce margin for smaller screens */
+    margin: 35px;
     display: inline-block;
     width: 90px;
     height: 90px;
-    fill: #4A5568; /* Default icon color (gray) */
+    fill: white; /* Change icon color to white */
     transition: fill 0.3s ease;
   }
 
   .social-icons a:hover {
-    fill: #1DA1F2; /* Change color on hover */
+    fill: #1e40af; /* Change hover color */
   }
 
   svg {
@@ -25,7 +25,6 @@
     height: 100%;
   }
 
-  /* Responsive text for the heading */
   .heading {
     margin-top: 150px;
     display: flex;
@@ -33,62 +32,63 @@
     align-items: center;
     padding: 20px;
     font-weight: bold;
-    color: blue;
-    font-size: 2rem; /* Default font size */
-    text-align: center; /* Center align text */
+    color: white; /* Change heading color to white */
+    font-size: 2rem; 
+    text-align: center; 
   }
 
   @media (max-width: 768px) {
     .heading {
-      font-size: 1.5rem; /* Adjust font size for tablets */
+      font-size: 1.5rem; 
     }
     
     .social-icons a {
-      width: 70px; /* Smaller icon size for mobile */
+      width: 70px; 
       height: 70px;
     }
   }
 
   @media (max-width: 480px) {
     .heading {
-      font-size: 1.2rem; /* Adjust font size for mobile */
+      font-size: 1.2rem; 
     }
 
     .social-icons a {
-      width: 60px; /* Smaller icon size for mobile */
+      width: 60px; 
       height: 60px;
     }
   }
 </style>
+<div style="background-color: #3f3f46; min-height: 100vh; padding: 20px;">
+  <div class="heading">
+    <h1>You may visit my accounts and message me by using these links below!</h1>
+  </div>
 
-<div class="heading">
-  <h1>You may visit my accounts and message me by using these links below!</h1>
-</div>
+  <div class="social-icons">
+    <a href="https://github.com/dupalaljhon" target="_blank" aria-label="GitHub">
+      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 0C5.37 0 0 5.37 0 12c0 5.303 3.438 9.8 8.207 11.387.6.11.793-.26.793-.577 0-.285-.01-1.04-.015-2.04-3.338.725-4.042-1.61-4.042-1.61-.546-1.387-1.334-1.757-1.334-1.757-1.09-.745.083-.729.083-.729 1.204.085 1.838 1.237 1.838 1.237 1.07 1.835 2.807 1.305 3.492.998.108-.776.418-1.305.762-1.605-2.665-.303-5.467-1.333-5.467-5.93 0-1.31.468-2.382 1.235-3.222-.123-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.301 1.23A11.516 11.516 0 0112 6.804c1.02.005 2.046.138 3.004.404 2.293-1.552 3.299-1.23 3.299-1.23.653 1.653.241 2.873.119 3.176.77.84 1.235 1.912 1.235 3.222 0 4.609-2.805 5.625-5.475 5.921.43.372.823 1.103.823 2.222 0 1.606-.015 2.902-.015 3.296 0 .32.192.694.798.576C20.565 21.796 24 17.298 24 12 24 5.37 18.63 0 12 0z"/>
+      </svg>
+    </a>
 
-<div class="social-icons">
-  <a href="https://github.com/dupalaljhon" target="_blank" aria-label="GitHub">
-    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path d="M12 0C5.37 0 0 5.37 0 12c0 5.303 3.438 9.8 8.207 11.387.6.11.793-.26.793-.577 0-.285-.01-1.04-.015-2.04-3.338.725-4.042-1.61-4.042-1.61-.546-1.387-1.334-1.757-1.334-1.757-1.09-.745.083-.729.083-.729 1.204.085 1.838 1.237 1.838 1.237 1.07 1.835 2.807 1.305 3.492.998.108-.776.418-1.305.762-1.605-2.665-.303-5.467-1.333-5.467-5.93 0-1.31.468-2.382 1.235-3.222-.123-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.301 1.23A11.516 11.516 0 0112 6.804c1.02.005 2.046.138 3.004.404 2.293-1.552 3.299-1.23 3.299-1.23.653 1.653.241 2.873.119 3.176.77.84 1.235 1.912 1.235 3.222 0 4.609-2.805 5.625-5.475 5.921.43.372.823 1.103.823 2.222 0 1.606-.015 2.902-.015 3.296 0 .32.192.694.798.576C20.565 21.796 24 17.298 24 12 24 5.37 18.63 0 12 0z"/>
-    </svg>
-  </a>
+    <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=202011035@gordoncollege.edu.ph" target="_blank" aria-label="Gmail">
+      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 50 50">
+        <path d="M 5.5 7 C 3.019531 7 1 9.019531 1 11.5 L 1 11.925781 L 25 29 L 49 11.925781 L 49 11.5 C 49 9.019531 46.980469 7 44.5 7 Z M 6.351563 9 L 43.644531 9 L 25 22 Z M 1 14.027344 L 1 38.5 C 1 40.980469 3.019531 43 5.5 43 L 44.5 43 C 46.980469 43 49 40.980469 49 38.5 L 49 14.027344 L 43 18.296875 L 43 41 L 7 41 L 7 18.296875 Z"></path>
+      </svg>
+    </a>
 
-  <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=202011035@gordoncollege.edu.ph" target="_blank" aria-label="Gmail">
-    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 50 50">
-      <path d="M 5.5 7 C 3.019531 7 1 9.019531 1 11.5 L 1 11.925781 L 25 29 L 49 11.925781 L 49 11.5 C 49 9.019531 46.980469 7 44.5 7 Z M 6.351563 9 L 43.644531 9 L 25 22 Z M 1 14.027344 L 1 38.5 C 1 40.980469 3.019531 43 5.5 43 L 44.5 43 C 46.980469 43 49 40.980469 49 38.5 L 49 14.027344 L 43 18.296875 L 43 41 L 7 41 L 7 18.296875 Z"></path>
-    </svg>
-  </a>
+    <a href="https://www.facebook.com/profile.php?id=100007309275458&mibextid=ZbWKwL" target="_blank" aria-label="Facebook">
+      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M22.675 0H1.325C.593 0 0 .593 0 1.326v21.348C0 23.407.593 24 1.325 24H12.82v-9.294H9.692V10.62h3.128V8.413c0-3.1 1.892-4.788 4.657-4.788 1.325 0 2.463.099 2.797.143v3.24h-1.918c-1.505 0-1.796.715-1.796 1.763v2.31h3.587l-.467 3.086h-3.12V24h6.116c.73 0 1.324-.593 1.324-1.326V1.326C24 .593 23.407 0 22.675 0z"/>
+      </svg>
+    </a>
 
-  <a href="https://www.facebook.com/profile.php?id=100007309275458&mibextid=ZbWKwL" target="_blank" aria-label="Facebook">
-    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path d="M22.675 0H1.325C.593 0 0 .593 0 1.326v21.348C0 23.407.593 24 1.325 24H12.82v-9.294H9.692V10.62h3.128V8.413c0-3.1 1.892-4.788 4.657-4.788 1.325 0 2.463.099 2.797.143v3.24h-1.918c-1.505 0-1.796.715-1.796 1.763v2.31h3.587l-.467 3.086h-3.12V24h6.116c.73 0 1.324-.593 1.324-1.326V1.326C24 .593 23.407 0 22.675 0z"/>
-    </svg>
-  </a>
-
-  <a href="https://t.me/Aljhondupal1023" target="_blank" aria-label="Telegram">
-    <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-      <g data-name="telegram social media network chat" id="telegram_social_media_network_chat">
-        <path d="M28.59,4.29a2.23,2.23,0,0,0-2.27-.36L3.41,13.1a1.83,1.83,0,0,0,0,3.38l1.48.61a1,1,0,0,0,1.31-.53,1,1,0,0,0-.54-1.31L4.56,14.8l22.51-9a.22.22,0,0,1,.23,0,.24.24,0,0,1,.08.23L23.27,25.21a.4.4,0,0,1-.26.3.39.39,0,0,1-.39-.06l-8-6.24,7.83-7.91a1,1,0,0,0-1.22-1.56L9.75,16.54a1,1,0,1,0,1,1.72l4.83-2.85L13.23,17.8a2,2,0,0,0,.2,3.08l8,6.15a2.4,2.4,0,0,0,1.47.5,2.47,2.47,0,0,0,.83-.15,2.37,2.37,0,0,0,1.52-1.75L29.33,6.47A2.23,2.23,0,0,0,28.59,4.29Z"/>
-      </g>
-    </svg>
-  </a>
+    <a href="https://t.me/Aljhondupal1023" target="_blank" aria-label="Telegram">
+      <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+        <g data-name="telegram social media network chat" id="telegram_social_media_network_chat">
+          <path d="M28.59,4.29a2.23,2.23,0,0,0-2.27-.36L3.41,13.1a1.83,1.83,0,0,0,0,3.38l1.48.61a1,1,0,0,0,1.31-.53,1,1,0,0,0-.54-1.31L4.56,14.8l22.51-9a.22.22,0,0,1,.23,0,.24.24,0,0,1,.08.23L23.27,25.21a.4.4,0,0,1-.26.3.39.39,0,0,1-.39-.06l-8-6.24,7.83-7.91a1,1,0,0,0-1.22-1.56L9.75,16.54a1,1,0,1,0,1,1.72l4.83-2.85L13.23,17.8a2,2,0,0,0,.2,3.08l8,6.15a2.4,2.4,0,0,0,1.47.5,2.47,2.47,0,0,0,.83-.15,2.37,2.37,0,0,0,1.52-1.75L29.33,6.47A2.23,2.23,0,0,0,28.59,4.29Z"/>
+        </g>
+      </svg>
+    </a>
+  </div>
 </div>
